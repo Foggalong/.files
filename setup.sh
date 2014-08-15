16ac88945230f7d1451f31c9679e04b25de6ef77
Joshua Fogg <joshua.h.fogg@gmail.com>
2014-08-15 15:25:56 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,6 +20,11 @@ add-apt-repository ppa:ricotz/docky
 apt-get update
 apt-get install plank
 
+# Updates whisker menu
+add-apt-repository ppa:gottcode/gcppa
+apt-get update
+apt-get install xfce4-whiskermenu-plugin
+
 # Installs browsers
 apt-get install links midori
 wget http://deb.opera.com/opera-developer/pool/non-free/o/opera-developer/opera-developer_25.0.1592.0_amd64.deb
@@ -101,3 +106,74 @@ apt-get install gdebi
 wget http://media.steampowered.com/client/installer/steam.deb
 dpkg -i steam.deb && apt-get -f install
 rm steam.deb
+
+# Sets up notes
+rm -rf /home/josh/.local/share/notes/Notes
+ln -s -r /home/josh/ownCloud/Notes/ /home/josh/.local/share/notes
+
+# Sets up Music
+rm -rf /home/josh/Music
+ln -s -r /home/josh/ownCloud/music/ /home/josh/Music
+
+# Sets up Documents
+ln -s -r /home/josh/ownCloud/documents/ /home/josh/Documents/Synced
+
+# Sets up Programs
+mkdir /home/josh/Programs
+
+# Sets up Web
+cd /home/josh/Programs
+mkdir Web && cd Web
+git clone https://github.com/Foggalong/fogg.me.uk.git
+git clone https://github.com/Foggalong/foggalong.github.io.git
+git clone https://github.com/numixproject/numixproject.github.io.git
+
+# Sets up Icons
+cd /home/josh/Programs
+mkdir Icons && cd Icons
+# Sets up Numix
+mkdir Numix && cd Numix
+mkdir Desktop && cd Desktop
+git clone https://github.com/numixproject/numix-folders.git
+git clone https://github.com/numixproject/numix-icon-theme.git
+git clone https://github.com/numixproject/numix-icon-theme-circle.git
+git clone https://github.com/numixproject/numix-icon-theme-bevel.git
+git clone https://github.com/numixproject/numix-icon-theme-utouch.git
+git clone https://github.com/numixproject/numix-icon-theme-shine.git
+git clone https://gitlab.com/numix/numix-icon-theme-hexagon.git
+git clone https://gitlab.com/numix/icon-theme-fold.git
+git clone https://gitlab.com/numix/numix-icon-theme-cookie.git
+git clone https://gitlab.com/numix/numix-icon-theme-square.git
+ln -s -r numix-icon-theme/Numix/ /usr/share/icons/
+ln -s -r numix-icon-theme-circle/Numix-Circle/ /usr/share/icons/
+ln -s -r numix-icon-theme-bevel/Numix-Bevel/ /usr/share/icons/
+ln -s -r numix-icon-theme-utouch/Numix-uTouch/ /usr/share/icons/
+ln -s -r numix-icon-theme-shine/Numix-Shine/ /usr/share/icons/
+ln -s -r numix-icon-theme-hexagon/Numix-Hexagon/ /usr/share/icons/
+ln -s -r numix-icon-theme-cookie/Numix-Cookie/ /usr/share/icons/
+ln -s -r numix-icon-theme-square/Numix-Square/ /usr/share/icons/
+cd ../ && mkdir Android && cd Android
+git clone https://github.com/numixproject/com.numix.icons_circle.git
+git clone https://gitlab.com/numix/com-numix-icons_hexagon.git
+git clone https://gitlab.com/numix/com-numix-icons_square.git
+git clone https://gitlab.com/numix/fold-for-android.git
+cd ../
+git clone https://gitlab.com/numix/script-o-mighty.git
+git clone https://github.com/numixproject/numix-labs.git
+git clone https://github.com/numixproject/numix-wiki.git
+git clone https://github.com/numixproject/numix-tools.git
+git clone https://github.com/numixproject/numix-assets.git
+cd ../
+# Sets up Moka
+mkdir Moka && cd Moka
+git clone https://github.com/moka-project/moka-icon-theme.git
+git clone https://github.com/moka-project/faba-icon-theme.git
+ln -s -r moka-icon-theme/Moka/ /usr/share/icons
+ln -s -r faba-icon-theme/Faba/ /usr/share/icons
+cd ../
+# Sets up Nitrux
+mkdir Nitrux && cd Nitrux
+git clone https://github.com/NitruxSA/flattr-icons.git
+ln -s -r flattr-icons /usr/share/icons
+cd ../
+cd ../
