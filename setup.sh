466ad3f6de08af0d3f4a13e9e36ea993f32d5a61
Joshua Fogg <joshua.h.fogg@gmail.com>
2014-08-15 13:14:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,15 +12,18 @@ apt-get upgrade
 apt-get dist-upgrade
 apt-get autoremove
 
-# Install and configure git
-apt-get install git
-git config --global user.name "Joshua Fogg"
-git config --global user.email "joshua.h.fogg@gmail.com"
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=14400'
-
 # Checks 'wget' is installed
 apt-get install wget
+
+# Installs browsers
+apt-get install links midori
+wget http://deb.opera.com/opera-developer/pool/non-free/o/opera-developer/opera-developer_25.0.1592.0_amd64.deb
+dpkg -i opera-developer_25.0.1592.0_amd64.deb && apt-get -f install
+rm *.deb
+add-apt-repository ppa:ubuntu-mozilla-daily/firefox-aurora
+apt-get update
+apt-get install firefox
+apt-get upgrade
 
 # Installs ownCloud client
 wget http://launchpadlibrarian.net/161423097/libowncloudsync0_1.5.0%2Bdfsg-4ubuntu1_amd64.deb
@@ -28,8 +31,29 @@ wget http://launchpadlibrarian.net/161423098/owncloud-client-cmd_1.5.0%2Bdfsg-4u
 wget http://launchpadlibrarian.net/161423096/owncloud-client_1.5.0%2Bdfsg-4ubuntu1_amd64.deb
 wget http://launchpadlibrarian.net/161423385/owncloud-client-doc_1.5.0%2Bdfsg-4ubuntu1_all.deb
 wget http://launchpadlibrarian.net/161423388/owncloud-client-l10n_1.5.0%2Bdfsg-4ubuntu1_all.deb
-dpkg -i owncloud-client-l10n_1.5.0+dfsg-4ubuntu1_all.deb
-dpkg -i libowncloudsync0_1.5.0+dfsg-4ubuntu1_amd64.deb
-dpkg -i owncloud-client-doc_1.5.0+dfsg-4ubuntu1_all.deb
-dpkg -i owncloud-client_1.5.0+dfsg-4ubuntu1_amd64.deb
-dpkg -i owncloud-client-cmd_1.5.0+dfsg-4ubuntu1_amd64.deb
+dpkg -i owncloud-client-l10n_1.5.0+dfsg-4ubuntu1_all.deb && apt-get -f install
+dpkg -i libowncloudsync0_1.5.0+dfsg-4ubuntu1_amd64.deb && apt-get -f install
+dpkg -i owncloud-client-doc_1.5.0+dfsg-4ubuntu1_all.deb && apt-get -f install
+dpkg -i owncloud-client_1.5.0+dfsg-4ubuntu1_amd64.deb && apt-get -f install
+dpkg -i owncloud-client-cmd_1.5.0+dfsg-4ubuntu1_amd64.deb && apt-get -f install
+rm *.deb
+
+# Install and configure git
+apt-get install git
+git config --global user.name "Joshua Fogg"
+git config --global user.email "joshua.h.fogg@gmail.com"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=14400'
+
+# Installs terminator
+apt-get install terminator
+
+# Installs office
+apt-get install libreoffice
+apt-get remove abiword
+apt-get remove gnumeric
+
+# Installs Steam
+wget http://media.steampowered.com/client/installer/steam.deb
+dpkg -i steam.deb && apt-get -f install
+rm steam.deb
